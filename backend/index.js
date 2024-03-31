040afd960c713896a68976c3d5de493739450dd0
BIMA-25 <bima.bagus.25@gmail.com>
2024-04-01 00:16:05 +0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import UserRoute from "./routes/UserRoute.js"
+
+const app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/hutangApp',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('database terkoneksi weeeekkk..'));
+
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute);
+
+app.listen(5001, ()=> console.log('server berjalann jiaaa'));
